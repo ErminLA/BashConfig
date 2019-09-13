5f7ba9cb503f3fb18f151ccca051026cd392dc74
ErminLA <42754356+ErminLA@users.noreply.github.com>
2019-09-13 04:03:02 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,10 +2,10 @@
 
 echo START_OF_SCRIPT >> /home/cloud_user/MyLog
 
-firewall-cmd --add-port=80/tcp --permanent
+firewall-offline-cmd --add-port=80/tcp --permanent
 echo PORT_80 >> /home/cloud_user/MyLog
 
-firewall-cmd --add-port=22/tcp --permanent
+firewall-offline-cmd --add-port=22/tcp --permanent
 echo PORT_22 >> /home/cloud_user/MyLog
 
 firewall-cmd --reload
